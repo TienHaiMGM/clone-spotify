a66e66edd01f71280068a9f41491a34b5258d105
TienHaiMGM <nguyentienhai465@gmail.com>
2022-11-24 17:11:00 +0700

    update play album

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { useSelector, useDispatch } from 'react-redux';
-import { login, logout, getUsers } from './redux/features/loginSlice';
-import { getCategories, getCategorie, getAlbums } from './redux/features/categoriesSlice'
-import { getCurrentlyPlaying, getPlayer } from './redux/features/currentlyPlayingSlice'
+import React, { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useSelector, useDispatch } from "react-redux";
+import { login, logout, getUsers } from "./redux/features/loginSlice";
+import {
+  getCategories,
+  getCategorie,
+  getAlbums,
+  getArtist,
+} from "./redux/features/categoriesSlice";
+import {
+  getCurrentlyPlaying,
+  getPlayer,
+} from "./redux/features/currentlyPlayingSlice";
 
-import Header from './components/Header';
-import Playlists from './pages/Playlists';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-import './App.css';
-import { getTokenFromUrl } from './data/spotify';
-import PopupLogin from './components/PopupLogin';
-
-
+import Header from "./components/Header";
+import HomePlaylists from "./pages/HomePlaylists";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import { getTokenFromUrl } from "./data/spotify";
+import PopupLogin from "./components/PopupLogin";
 
 function App() {
   const [token, setToken] = useState();
 
-  const stateLogin = useSelector((state) => state.loginReducer)
-  const stateCategories = useSelector((state) => state.categoriesReducer)
+  const stateLogin = useSelector((state) => state.loginReducer);
+  const stateCategories = useSelector((state) => state.categoriesReducer);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -33,19 +39,27 @@ function App() {
       dispatch(getUsers({ _token }));
     }
     dispatch(getCategories());
+    dispatch(getArtist());
     dispatch(getCategorie());
     dispatch(getAlbums());
     dispatch(getCurrentlyPlaying());
     dispatch(getPlayer());
-  }, [])
+  }, []);
 
   return (
     <div className="home">
-      <nav><Navbar /></nav>
-      <header><Header ></Header ></header>
-      <main><Playlists >
-      </Playlists ></main>
-      <footer><Footer /></footer>
+      <nav>
+        <Navbar />
+      </nav>
+      <header>
+        <Header></Header>
+      </header>
+      <main>
+        <HomePlaylists></HomePlaylists>
+      </main>
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Provider } from 'react-redux'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
 
-import store from './redux/store'
-import reportWebVitals from './reportWebVitals';
-import './index.css';
-import App from './App';
-import Search from './pages/Search';
-import YourLibrary from './pages/YourLibrary';
-import LikedSongs from './pages/LikedSongs';
-import Playlist from './pages/Playlist';
-import Album from './pages/Album';
-import Episode from './pages/Episode'
-import Track from './pages/Track'
-import Show from './pages/Show'
+import store from "./redux/store";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import App from "./App";
+import Search from "./pages/Search";
+import YourLibrary from "./pages/YourLibrary";
+import LikedSongs from "./pages/LikedSongs";
+import Playlist from "./pages/Playlist";
+import Album from "./pages/Album";
+import Episode from "./pages/Episode";
+import Track from "./pages/Track";
+import Show from "./pages/Show";
+import Artist from "./pages/Artist";
 
 const router = createBrowserRouter([
   {
@@ -38,8 +39,12 @@ const router = createBrowserRouter([
     element: <Playlist />,
   },
   {
-    path: "/album/:albumtId",
+    path: "/album/:albumId",
     element: <Album />,
+  },
+  {
+    path: "/artist/:artistId",
+    element: <Artist />,
   },
   {
     path: "/track/:trackId",
@@ -53,14 +58,13 @@ const router = createBrowserRouter([
     path: "/episode/:episodetId",
     element: <Episode />,
   },
+]);
 
-])
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
     <RouterProvider router={router} />
-  </Provider >,
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
